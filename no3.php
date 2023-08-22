af6b75778bede650ade5fa959b2beb4146976020
MArdanFadhil <pentingnyamemahi@gmail.com>
2023-08-22 10:50:11 +0700

    phpDasar

---
+++ no3.php
@@ -0,0 +1,10 @@
+<?php
+    $j;
+    $m;
+    $d;
+    $td;
+
+    $td = $j * 3600 + $m * 60 + $d;
+    echo "hasilnya" . $td;
+
+?>
---
+++ no5.php
@@ -0,0 +1,9 @@
+<?php
+
+    $ganjil;
+    $genap;
+    $nilai;
+
+    for($i = 0; $i < $nilai; i++)
+
+?>
